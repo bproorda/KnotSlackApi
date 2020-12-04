9ffa9e2c0457e09c4526445ebccb2dd596c26d23
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-12-04 10:27:28 -0600

    fixed error for creating guest messages

--- signalrApi/signalrApi/Models/Message.cs
+++ signalrApi/signalrApi/Models/Message.cs
@@ -11,7 +11,7 @@ namespace signalrApi.Models
     {   [Key]
         public int Id { get; set; }
 
-        public string UserId { get; set; }
+        public string? UserId { get; set; }
 
         public string Sender { get; set; }
 
